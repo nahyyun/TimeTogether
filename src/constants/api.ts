b52c35c114177443c825c86c32dc8fa4c20a19be
nahyyun <nahyuun@naver.com>
2023-12-12 01:52:41 +0900

    fix: page extension 수정

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -8,7 +8,7 @@ export const END_POINT = {
 
 export const API_ERROR_MESSAGE = {
   400: "잘못된 요청입니다.",
-  404: "요청한 데이터 찾을 수 없습니다. 이전 페이지로 이동합니다.",
+  404: "요청한 데이터 찾을 수 없습니다.",
   NOT_FOUND_USER: "유저 정보가 존재하지 않습니다. 이전 페이지로 이동합니다.",
   NOT_FOUND_MEETING: "일정 정보가 존재하지 않습니다.",
   500: "서버 오류가 발생했습니다.",
