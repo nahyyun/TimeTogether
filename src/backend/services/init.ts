8312fea376ae7540ecd29d7a136e73dfe0d280ac
nahyyun <nahyuun@naver.com>
2024-06-14 10:16:22 +0900

    fix: persistSession 옵션 추가

--- src/backend/services/init.ts
+++ src/backend/services/init.ts
@@ -1,9 +1,11 @@
+import { createClient } from '@supabase/supabase-js';
+
 import { Database } from "@/types/supabase";
-import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  {auth: {persistSession: false}}
 );
 
 export default supabase;
